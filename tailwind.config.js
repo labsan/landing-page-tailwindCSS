369e6d7d86460bf5b9832c000cbce1864951be52
labsan <yondeveloper@gmail.com>
2023-02-09 23:33:01 +0700

    💄 Update custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Satoshi", "cursive"],
+        satoshi: ['satoshi', 'cursive', 'sans-serif'],
       },
       colors: {
         primaryOne: "#003E29",
@@ -12,7 +12,7 @@ module.exports = {
         primaryThree: "#D9D9D9",
         borderColor: "#787878",
         bgColor: "#EDEDED",
-        bgFooter: "#023423"
+        bgFooter: "#023423" 
       },
     },
   },
